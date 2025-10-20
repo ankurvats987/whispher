59c5eaf569ed4d6ecade8e773ef7a053987f1a58
ANKUR <rockingjohnny2004@gmail.com>
2025-10-20 19:48:46 +0530

    fix: make the button show disable cursor on hover

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -10,7 +10,7 @@ export const Button = ({
   return (
     <button
       disabled={isDisabled}
-      className={`cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 ${extraStyle}`}
+      className={`inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 ${extraStyle}`}
       onClick={onClick}
     >
       {title === "" ? children : title}
