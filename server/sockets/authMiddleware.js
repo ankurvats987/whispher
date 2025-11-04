e5c4b51a26080555131735329369016c03874dcd
ANKUR <rockingjohnny2004@gmail.com>
2025-11-04 14:15:45 +0530

    fix: surround socket auth middleware with try/catch

--- server/sockets/authMiddleware.js
+++ server/sockets/authMiddleware.js
@@ -2,13 +2,21 @@ import jwt from "jsonwebtoken";
 
 export default function authMiddleware(io) {
   io.use((socket, next) => {
-    const token = socket.handshake.auth?.token;
-    if (!token) return next(new Error("Authentication required"));
+    try {
+      const token = socket.handshake.auth?.token;
+      if (!token) return next(new Error("Authentication required"));
 
-    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    if (!user) return next(new Error("Invalid or expired token"));
+      const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      if (!user) return next(new Error("Invalid or expired token"));
 
-    socket.user = user;
-    next();
+      socket.user = user;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return next(new Error("Token Expired"));
+      } else {
+        return next(new Error("Invalid token"));
+      }
+    }
   });
 }
